5b168808c66a97d1d77c8537ce378d6a33301c97
unknown <ruiz.james12@yahoo.com>
2019-06-03 21:56:44 -0700

    Retrieved meta data on quotes

--- scraping.py
+++ scraping.py
@@ -4,3 +4,11 @@ import csv
 
 response = requests.get('http://quotes.toscrape.com/')
 soup = BeautifulSoup(response.text, 'html.parser')
+
+quotes_list = []
+quotes = soup.find_all(class_="quote")
+for quote in quotes:
+    quotes_list.append({
+        "text": quote.find(class_="text").get_text()
+        "author": quote.find(class_="author").get_text()
+    })
